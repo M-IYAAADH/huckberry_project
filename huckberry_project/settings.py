d150581c4bad8b16545efb35ebbc2019ab5b5be0
M-IYAAADH <mohamediyaadh144@gmail.com>
2025-12-07 13:59:47 +0500

    Prepare for Render deployment (static OK)

--- huckberry_project/settings.py
+++ huckberry_project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-l$me^yr94!=#v*cdzx0$eg$w_$0t#@fz#r(i%u+2hrzz83q%um'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -101,10 +102,18 @@ USE_TZ        = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Where Render will collect static files to serve in production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# This stays ONLY if you have a static/ folder for development
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# WhiteNoise for serving static files in production
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Media (user-uploaded files)
 MEDIA_URL  = '/media/'
